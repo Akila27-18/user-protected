597a6e6bf82beaeb14dd9c185e4f71bd52aec775
Akila <akila271819@gmail.com>
2025-09-15 01:09:13 +0530

    settings

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.http import JsonResponse
+from api.views import ProtectedView
 
 def home(request):
     return JsonResponse({"message": "Backend is running 🚀"})
 
 urlpatterns = [
-    path("", home),  # <--- add this
+    path("", home),  # root route
+    path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/protected/", ProtectedView.as_view(), name="protected"),
